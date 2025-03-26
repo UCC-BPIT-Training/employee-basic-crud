d746298af5e3dca16f914530a3d82fb7a8effe74
Jay Are Galinada <jayaregalinada@gmail.com>
2025-03-26 16:13:16 +0800

    fix: removed comments

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// import express
 import express from 'express';
 import bodyParser from 'body-parser';
-// initialize the express app
+
 const app = express();
 
-// In middleware the order/sequence is important
 app.use(bodyParser.json());
 
-// what port to use
 app.listen(process.env.PORT || 3000, () => {
   console.log('Service is running');
 });
